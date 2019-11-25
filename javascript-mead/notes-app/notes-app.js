d64854ac1c1fca6b0b2059523a26339955742b8e
Allie Robinson <allie-rae@Allies-MacBook.local>
2019-11-25 12:54:23 -0600

    Incomplete notes app and todo app made with javascript

---
+++ javascript-mead/notes-app/notes-app.js
@@ -0,0 +1,47 @@
+const notes = [
+    {
+        title: 'Colorado',
+        body: 'So many cute animals!'
+    },
+    {
+        title: 'New Orleans',
+        body: 'Had a bad time tbh'
+    },
+    {
+        title: 'Indiana',
+        body: 'Cute boyfriend shows me around his past'
+    },
+    {
+        title: 'Portland',
+        body: 'So cute! Giant bookstore. GF fish & chips.'
+    }
+]
+
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function (note) {
+        const noteElement = document.createElement('p')
+        noteElement.textContent = note.title
+        document.querySelector('#notes').appendChild(noteElement)
+    })
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#addNote').addEventListener('click', function () {
+    console.log("Click")
+})
+
+document.querySelector('#noteInput').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
---
+++ javascript-mead/todo-app/todo-app.js
@@ -0,0 +1,62 @@
+const todos = [
+    {
+        text: 'Take a bath',
+        completed: true
+    },
+    {
+        text: 'Go on a walk',
+        completed: true
+    },
+    {
+        text: 'Finish this course',
+        completed: false
+    },
+    {
+        text: 'Complete code challenges',
+        completed: false
+    },
+    {
+        text: 'Sing a song',
+        completed: false
+    }
+]
+
+const todoDiv = document.querySelector('#todos')
+
+const filters = {
+    searchText: ''
+}
+
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    const todosRemaining = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${todosRemaining.length} things left to do.`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const todoElement = document.createElement('p')
+        todoElement.textContent = todo.text
+        document.querySelector('#todos').appendChild(todoElement)
+    })
+}
+
+renderTodos(todos, filters)
+
+
+document.querySelector('.addTodo').addEventListener('click', function () {
+    console.log('I was clicked!')
+})
+
+document.querySelector('#todoSearch').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})

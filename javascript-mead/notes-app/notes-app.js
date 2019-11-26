7b5092f5d59661ece9857ff5da555fa2a61bb4aa
Allie Robinson <allie-rae@Allies-MacBook.local>
2019-11-26 02:30:58 -0600

    Learned about localstorage and JSON

--- javascript-mead/notes-app/notes-app.js
+++ javascript-mead/notes-app/notes-app.js
@@ -17,6 +17,27 @@ const notes = [
     }
 ]
 
+// localStorage.setItem('location', 'Seattle') - puts data in storage
+
+// console.log(localStorage.getItem('location')) - reads data
+
+// localStorage.removeItem('location') - removes specific data
+
+// localstorage.clear() - removes ALL saved data
+
+// const user = {
+//     name: 'Allie',
+//     age: 26
+// }
+
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
+
 const filters = {
     searchText: ''
 }
